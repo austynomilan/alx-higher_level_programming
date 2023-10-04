5a81b6d7146d49c14a4e1fd5e3d0bf604f4cc2ff
Augustine <austynomilano77@gmail.com>
2023-10-04 15:43:15 -0700

    positive and negative

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ if number < 0:
     last_digit = number % -10
 else:
     last_digit = number % 10
-print(f"Last digit of {number} is {last_digit} ", end = "")
+print(f"Last digit of {number} is {last_digit} ", end=" ")
 if last_digit == 0:
     print(f"and is 0")
 elif last_digit > 5:
