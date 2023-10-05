8b19fbbd46f448177e31121d12c579fcd8779106
Augustine <austynomilano77@gmail.com>
2023-10-04 19:10:56 -0700

    byte codes

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-def remove_char_at(str, n):
-    if 0 <= n < len(str):
-        return (str.replace(str[n], ""))
+def magic_calculation(a, b, c):
+    if a < b:
+        return (c)
+    elif c > b:
+        return (a + b)
     else:
-        return (str)
+        return (a* b - c)
