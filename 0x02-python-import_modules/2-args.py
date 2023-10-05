9f7c08edfe0ff5e60d2f6886fda0841cb287beca
Augustine <austynomilano77@gmail.com>
2023-10-05 09:16:11 -0700

    arg count and list

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    i = 0
     argcont = len(sys.argv) - 1
     if argcont == 1:
         print("1 argument", end="")
-    elif argcont != 1:
+    else:
         print("{} arguments".format(argcont), end="")
+    if argcont > 0:
+        print(":", end="")
+    else:
+        print(".", end="")
+
+    print()
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        print("{}: {}".format(i, arg))
