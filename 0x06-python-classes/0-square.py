1b8eac831523a76b25ce0693b94855d6c3f91214
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2023-10-26 00:13:43 +0100

    class initialized

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
-
 class Square:
-    """
-    empty class for Square
-    """
-    pass
+    """square"""
+    def __init__(self):
+        """initialize square attributes"""
+        return None
